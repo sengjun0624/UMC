90ad7dbfce160e09dd5d1f3b7ae69b0bae420787
sengjun0624 <sengjun0624@naver.com>
2023-11-01 18:35:34 +0900

    feat : make MemberEntity to make MemberTable

---
+++ src/main/java/com/example/umc/domain/MemberEntity.java
@@ -0,0 +1,35 @@
+package com.example.umc.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.time.LocalDate;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class MemberEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    private String address;
+
+    private String specAddress;
+
+    private LocalDate inactiveDate;
+
+    private String email;
+
+    private Integer point;
+}
---
+++ src/main/java/com/example/umc/domain/enums/Gender.java
@@ -0,0 +1,4 @@
+package com.example.umc.domain.enums;
+
+public enum Gender {
+}
