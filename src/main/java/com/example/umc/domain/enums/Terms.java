84d000272ea8671e77b73e359d584f08ebc8ce24
sengjun0624 <sengjun0624@naver.com>
2023-11-01 18:41:36 +0900

    feat: add Terms

---
+++ src/main/java/com/example/umc/domain/enums/Terms.java
@@ -0,0 +1,29 @@
+package com.example.umc.domain.enums;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Terms {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String body;
+
+    private Boolean optional;
+}
