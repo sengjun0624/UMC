a147095be58446a489187cbdde635cdbf8712b2c
sengjun0624 <sengjun0624@naver.com>
2023-11-01 19:18:29 +0900

    feat : add mapping table and mission state enum class

---
+++ src/main/java/com/example/umc/domain/mapping/MemberAgree.java
@@ -0,0 +1,25 @@
+package com.example.umc.domain.mapping;
+
+import com.example.umc.domain.base.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class MemberAgree extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+}
---
+++ src/main/java/com/example/umc/domain/mapping/MemberMission.java
@@ -0,0 +1,29 @@
+package com.example.umc.domain.mapping;
+
+import com.example.umc.domain.base.BaseEntity;
+import com.example.umc.domain.enums.MissionStatus;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class MemberMission extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private MissionStatus status;
+}
