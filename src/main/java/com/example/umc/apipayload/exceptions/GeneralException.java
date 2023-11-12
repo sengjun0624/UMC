8b9e4142f760b1fd2bd013051c48be29540b0539
sengjun0624 <sengjun0624@naver.com>
2023-11-12 15:45:49 +0900

    feat(GeneralException): add General Excetption to thorw extions in here

---
+++ src/main/java/com/example/umc/apipayload/exceptions/GeneralException.java
@@ -0,0 +1,21 @@
+package com.example.umc.apipayload.exceptions;
+
+import com.example.umc.apipayload.code.BaseErrorCode;
+import com.example.umc.apipayload.code.ErrorReasonDTO;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class GeneralException extends RuntimeException {
+
+    private BaseErrorCode code;
+
+    public ErrorReasonDTO getErrorReason() {
+        return this.code.getReason();
+    }
+
+    public ErrorReasonDTO getErrorReasonHttpStatus() {
+        return this.code.getReasonHttpStatus();
+    }
+}
