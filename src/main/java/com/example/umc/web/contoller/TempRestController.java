e8fd69ddf2c9d8edb7b04880f10fb807246457f1
sengjun0624 <sengjun0624@naver.com>
2023-11-12 15:43:40 +0900

    feat(TempRestController): add Controler to process Temp request

---
+++ src/main/java/com/example/umc/web/contoller/TempRestController.java
@@ -0,0 +1,30 @@
+package com.example.umc.web.contoller;
+
+import com.example.umc.apipayload.ApiResponse;
+import com.example.umc.converter.TempConverter;
+import com.example.umc.service.TempQueryService;
+import com.example.umc.web.dto.TempResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/temp")
+@RequiredArgsConstructor
+public class TempRestController {
+    private final TempQueryService tempQueryService;
+
+    @GetMapping("/test")
+    public ApiResponse<TempResponse.TempTestDTO> testAPI() {
+
+        return ApiResponse.onSuccess(TempConverter.toTempTestDTO());
+    }
+
+    @GetMapping("/exception")
+    public ApiResponse<TempResponse.TempExceptionDTO> exceptionAPI(@RequestParam Integer flag) {
+        tempQueryService.CheckFlag(flag);
+        return ApiResponse.onSuccess(TempConverter.toTempExceptionDTO(flag));
+    }
+}
