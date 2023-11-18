303e1ea62f0df98779adc7e011533c6c3f795b78
이승준 <sengjun0624@naver.com>
2023-11-19 03:16:11 +0900

    :sparkles: feat(creat): add method to controller post request

--- src/main/java/com/example/umc/web/controller/store/StoreRestController.java
+++ src/main/java/com/example/umc/web/controller/store/StoreRestController.java
@@ -1,17 +1,22 @@
 package com.example.umc.web.controller.store;
 
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.umc.apipayload.ApiResponse;
 import com.example.umc.converter.store.StoreConverter;
+import com.example.umc.domain.Review;
 import com.example.umc.domain.Store;
 import com.example.umc.service.storeservice.StoreCommandService;
 import com.example.umc.web.dto.store.StoreRequestDTO;
 import com.example.umc.web.dto.store.StoreResponseDTO;
+import com.example.umc.web.dto.store.StoreReviewRequestDTO;
+import com.example.umc.web.dto.store.StoreReviewResponseDTO;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -31,5 +36,16 @@ public class StoreRestController {
         Store store = storeCommandService.SaveStore(request);
 
         return ApiResponse.onSuccess(StoreConverter.toStoreSaveResultDTO(store));
+    }
+
+    // :TODO: pathvariable이랑 QueryString에 커스텀 어노테이션 추가하기.
+    @PostMapping("/{storeId}/reviews")
+    public ApiResponse<StoreReviewResponseDTO.CreatReviewResultDTO> creat(
+            @RequestBody @Valid StoreReviewRequestDTO.CreatReviewDTO request,
+            @RequestParam(name = "memberId") Long memberId,
+            @PathVariable(name = "storeId") Long storeId) {
+
+        Review review = null;
+        return ApiResponse.onSuccess(StoreConverter.toCreatReviewResultDTO(review));
     }
 }
