e5297daf5592e40ce5185ee2c1741a735304add2
이승준 <sengjun0624@naver.com>
2023-11-19 01:46:21 +0900

    :sparkles: feat(StoreReviewRequestDTO):add RequestDTO for creat review

--- src/main/java/com/example/umc/web/dto/store/StoreReviewRequestDTO.java
+++ src/main/java/com/example/umc/web/dto/store/StoreReviewRequestDTO.java
@@ -1,4 +1,22 @@
 package com.example.umc.web.dto.store;
 
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+
 public class StoreReviewRequestDTO {
+
+    @Getter
+    public static class CreatReviewDTO {
+
+        @NotBlank private String body;
+
+        @DecimalMin("0")
+        @DecimalMax("5.0")
+        private float score;
+
+        @NotNull private Long storeId;
+    }
 }
