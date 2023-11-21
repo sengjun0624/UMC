6990638eb3679122d4eaf9ee2adca7805e0d5e47
이승준 <sengjun0624@naver.com>
2023-11-21 23:18:01 +0900

    :recycle: refactor(MemberMissionResponseDTO): refactor DTO add MissionStatus feild

--- src/main/java/com/example/umc/web/dto/mission/MemberMissionResponseDTO.java
+++ src/main/java/com/example/umc/web/dto/mission/MemberMissionResponseDTO.java
@@ -2,6 +2,8 @@ package com.example.umc.web.dto.mission;
 
 import java.time.LocalDateTime;
 
+import com.example.umc.domain.enums.MissionStatus;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -16,6 +18,8 @@ public class MemberMissionResponseDTO {
     public static class ChallengeResultDTO {
 
         private Long memberMissionId;
+
+        private MissionStatus status;
 
         private LocalDateTime createdAt;
     }
