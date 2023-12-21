184093e1289e2f49cc50c1a0749954ce99c5559d
이승준 <sengjun0624@naver.com>
2023-12-21 21:03:44 +0900

    :sparkles: Feat(StoreQuerryServiceImpl): add getReviewListImpl

--- src/main/java/com/example/umc/service/storeservice/StoreQueryServiceImpl.java
+++ src/main/java/com/example/umc/service/storeservice/StoreQueryServiceImpl.java
@@ -2,9 +2,11 @@ package com.example.umc.service.storeservice;
 
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.example.umc.domain.Review;
 import com.example.umc.domain.Store;
 import com.example.umc.repository.StoreRepository;
 
@@ -19,5 +21,10 @@ public class StoreQueryServiceImpl implements StoreQueryService {
     @Override
     public Optional<Store> isExistStore(Long storeId) {
         return storeRepository.findById(storeId);
+    }
+
+    @Override
+    public Page<Review> getReviewList(Long StoreId, Integer page) {
+        return null;
     }
 }
