05580f28e583eb04468df2939bc2141933fc2bf6
이승준 <sengjun0624@naver.com>
2023-12-21 21:04:51 +0900

    :sparkles: Feat(ReviewRepository ) add Repo method

--- src/main/java/com/example/umc/repository/ReviewRepository.java
+++ src/main/java/com/example/umc/repository/ReviewRepository.java
@@ -1,7 +1,12 @@
 package com.example.umc.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.umc.domain.Review;
+import com.example.umc.domain.Store;
 
-public interface ReviewRepository extends JpaRepository<Review, Long> {}
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+    Page<Review> findAllByStore(Store store, PageRequest pageRequest);
+}
